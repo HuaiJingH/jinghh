6c72aa960a4cc9a00a1182b8d01e464dfc170dfe
HuaiJingH <2680448137@qq.com>
2023-02-06 23:47:18 +0800

    function object

--- numerical_intergration.cpp
+++ numerical_intergration.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 const double euler_number=2.718281828459045;
 
+template<typename Function>
 class numerical_intergration
 {
 private:
@@ -23,6 +24,11 @@ private:
         return 0.5*(up-low)*((*func)(up)+(*func)(low));
     }
 
+    double trapezoid(double low,double up,Function f)       //以下有 Function f 参数的皆为函数对象重载
+    {
+        return 0.5*(up-low)*(f(up)+f(low));
+    }
+
     double trapezoid(double low,double up,int div)
     {
         double sum=0,h=(upper_bound-lower_bound)/div*1.0;
@@ -31,9 +37,22 @@ private:
         return sum;
     }
 
+    double trapezoid(double low,double up,int div,Function f)
+    {
+        double sum=0,h=(upper_bound-lower_bound)/div*1.0;
+        for(int i=0;i<div;i++)
+            sum+=trapezoid(lower_bound+i*h,lower_bound+(i+1)*h,f);
+        return sum;
+    }
+
     double simpson(double low,double up)
     {
         return (up-low)*((*func)(up)+(*func)(low)+4*(*func)((up+low)*0.5))/6.0;
+    }
+
+    double simpson(double low,double up,Function f)
+    {
+        return (up-low)*(f(up)+f(low)+4*f((up+low)*0.5))/6.0;
     }
 
     double simpson(double low,double up,int div)
@@ -41,6 +60,14 @@ private:
         double sum=0,h=(upper_bound-lower_bound)/div*1.0;
         for(int i=0;i<div;i++)
             sum+=simpson(lower_bound+i*h,lower_bound+(i+1)*h);
+        return sum;
+    }
+
+    double simpson(double low,double up,int div,Function f)
+    {
+        double sum=0,h=(upper_bound-lower_bound)/div*1.0;
+        for(int i=0;i<div;i++)
+            sum+=simpson(lower_bound+i*h,lower_bound+(i+1)*h,f);
         return sum;
     }
 
@@ -55,6 +82,20 @@ private:
         double sum=0,h=(upper_bound-lower_bound)/div*1.0;
         for(int i=0;i<div;i++)
             sum+=cotes(lower_bound+i*h,lower_bound+(i+1)*h);
+        return sum;
+    }
+
+    double cotes(double low,double up,Function f)
+    {
+        return 1.0/90*(up-low)*(7*(f(low)+f(up))+12*f((up+low)*0.5)
+               +32*(f(0.75*low+0.25*up)+f(0.25*low+0.75*up)));
+    }
+
+    double cotes(double low,double up,int div,Function f)
+    {
+        double sum=0,h=(upper_bound-lower_bound)/div*1.0;
+        for(int i=0;i<div;i++)
+            sum+=cotes(lower_bound+i*h,lower_bound+(i+1)*h,f);
         return sum;
     }
 
@@ -76,7 +117,25 @@ private:
         return a[count-1];
     }
 
-    double romberg(double low,double up,double pre)
+    double romberg(double low,double up,Function f)
+    {
+        double a[55];
+        int count=0;
+        double temp=1,temp_i=1;
+
+        a[count++]=trapezoid(low,up,temp_i,f);
+        for(int i=1;i<10;i++)
+            a[count++]=trapezoid(low,up,temp_i*=2,f);
+        for(int i=1;i<10;i++)
+            for(int j=0;j<9-i;j++)
+            {
+                temp*=4;
+                a[count++]=(temp*a[count-(10-i)]-a[count-(11-i)])/(temp-1);
+            }
+        return a[count-1];
+    }
+
+    double romberg(double low,double up,double pre)         //无函数对象重载
     {
         double a[55];
         int count=0;
@@ -129,11 +188,14 @@ private:
 
 public:
     numerical_intergration() {}
-    numerical_intergration(double (*f)(double x),int div=1):func{f},division{div},precision{-1} {}
     numerical_intergration(double (*f)(double x),double low,double up,int div=1)
     :func{f},upper_bound{up},lower_bound{low},division{div},precision{-1} {}
     numerical_intergration(double (*f)(double x),double low,double up,double pre)
     :func{f},upper_bound{up},lower_bound{low},precision{pre} {}
+    numerical_intergration(double low,double up,int div=1)
+    :func{nullptr},upper_bound{up},lower_bound{low},division{div},precision{-1} {}
+    numerical_intergration(double low,double up,double pre)
+    :func{nullptr},upper_bound{up},lower_bound{low},precision{pre} {}
 
     double trapezoid()
     {
@@ -141,6 +203,14 @@ public:
             return trapezoid(lower_bound,upper_bound);
         else
             return trapezoid(lower_bound,upper_bound,division);
+    }
+
+    double trapezoid(Function f)
+    {
+        if(division==1)
+            return trapezoid(lower_bound,upper_bound,f);
+        else
+            return trapezoid(lower_bound,upper_bound,division,f);
     }
 
     double simpson()
@@ -151,12 +221,28 @@ public:
             return simpson(lower_bound,upper_bound,division);
     }
 
+    double simpson(Function f)
+    {
+        if(division==1)
+            return simpson(lower_bound,upper_bound,f);
+        else
+            return simpson(lower_bound,upper_bound,division,f);
+    }
+
     double cotes()
     {
         if(division==1)
             return cotes(lower_bound,upper_bound);
         else
             return cotes(lower_bound,upper_bound,division);
+    }
+
+    double cotes(Function f)
+    {
+        if(division==1)
+            return cotes(lower_bound,upper_bound,f);
+        else
+            return cotes(lower_bound,upper_bound,division,f);
     }
 
     double romberg()
@@ -167,17 +253,44 @@ public:
             return romberg(lower_bound,upper_bound,precision);
     }
 
+    double romberg(Function f)
+    {
+        if(precision==-1)
+            return romberg(lower_bound,upper_bound,f);
+        /*
+        else
+            return romberg(lower_bound,upper_bound,precision);
+        */
+    }
+
 };
 
-double function(double x)
+template<typename T>
+class function
 {
+public:
+    T operator() (T x)
+    {
+        return 4.0/(1+x*x);
+    }
+};
+
+ double function_f(double x)
+ {
     return 4.0/(1+x*x);
-}
+ }
 
 int main()
 {
-    double (*fu)(double);
-    fu=function;
+    function<double> fu;    //函数对象方式
+    numerical_intergration<function<double>> cal(0,1);
+    cout<<setprecision(8)<<cal.trapezoid(fu)<<endl;
+    cout<<cal.simpson(fu)<<endl;
+    cout<<cal.cotes(fu)<<endl;
+
+    /*
+    double (*fu)(double x);    //函数指针方式
+    fu=function_f;
 
     clock_t start_2=clock();
     numerical_intergration cal2(fu,0,1,8);
@@ -190,7 +303,7 @@ int main()
     cout<<cal3.romberg()<<endl;
     clock_t end_3=clock();
     cout<<(double)(end_3-start_3)/CLOCKS_PER_SEC<<endl<<endl;
-
+    */
     system("pause");
     return 0;
 }
